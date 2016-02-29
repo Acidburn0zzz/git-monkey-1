e761b84fe3c380c64100c1c9783c8f4fd042c0fe
Louis-Bertrand Varin <louisbvarin@gmail.com>
2016-02-29 15:07:46 -0500

    Changes detection and ascii art.

--- git_monkey.sh
+++ git_monkey.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
+# Result variables.
+master_to_develop_files_changed=""
+merged_feature_branches=""
+
 repo_path=$1
 
-if [[ -a git_monkey.txt ]];
+git_monkey_ascii_art=$(dirname "$0")"/git_monkey.txt"
+if [[ -a $git_monkey_ascii_art ]];
 then
-  cat git_monkey.txt
+  cat $git_monkey_ascii_art
 fi
 
 if [[ -z "$repo_path" ]];
@@ -14,21 +19,25 @@ then
   exit 1
 fi
 
-pushd $repo_path
+pushd $repo_path >> /dev/null
 
 stash_content=$(git stash)
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 git checkout develop -q
-git pull origin develop -q
+git pull origin develop -q >> /dev/null
 
 git checkout master -q
-git pull origin master -q
+git pull origin master -q >> /dev/null
 
-echo "Printing list of changes in master not in develop"
+# Looking for changes in master but not in develop.
 for sha1 in $(git log --format=format:%H --no-merges develop..master); do
-  git diff $sha1 $sha1^1
+  for file_name in $(git show $sha1 --name-only --pretty=format:""); do
+    master_to_develop_files_changed=$master_to_develop_files_changed$file_name"\n"
+  done
 done
+master_to_develop_files_changed=$(echo -e $master_to_develop_files_changed | grep -vE "^[ \n]*$|^\.npmrc")
+
 
 git fetch -a --prune -q
 
@@ -51,4 +60,6 @@ then
   git stash pop -q
 fi
 
-popd
+echo -e $master_to_develop_files_changed
+
+popd >> /dev/null
